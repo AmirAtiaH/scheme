671a599ef8e07dc3eac65be42b7f0aefee6e7651
AmirAtiaH <amir.gppume@gmail.com>
2025-07-01 00:17:55 +0300

    hi

--- main.v
+++ main.v
@@ -8,17 +8,22 @@ import math
 fn main() {
 
 	code := os.read_file(os.args[1]) or { panic("can access the file") }
-	
-	start := time.now().unix_nano()
 
-	mut par := parser.Parser{
-		source: code
+	mut arr := []int
+
+	for 1 in 0..100 {
+		start := time.now().unix_nano()
+
+		mut par := parser.Parser{
+			source: code
+		}
+		par.parse()
+
+		close := time.now().unix_nano()
+
+		arr << (code.count('\n') / ((close - start) / math.pow(10, 9)))/ 1000
 	}
-	par.parse()
-
-	close := time.now().unix_nano()
-
-	//unsafe { free(ast) }
-	println((code.count('\n') / ((close - start) / math.pow(10, 9)))/ 1000 )
-	//println((close - start)/ 1000 )
+	
+	println( )
+	
 }
